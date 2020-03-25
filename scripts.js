0f10bffa62ffd080b173578e862ddb69eba437bc
Paulo Henrique <phenriq694@gmail.com>
2020-03-25 19:53:36 -0300

    Adicionando estilização no CSS para habliter e desabilitar o modal quando o botão 'adicioanr ideia' é clicado.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-document.querySelector("button.fat").addEventListener("click", () => {
-  document.querySelector("#footer").classList.toggle("hide")
-})
+function onOff() {
+  document
+  .querySelector("#modal")
+  .classList
+  .toggle("hide")
+}
